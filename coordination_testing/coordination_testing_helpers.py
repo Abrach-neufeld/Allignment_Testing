7a3f7d9292c4e998a6bd08d79f4f69616ae4734d
Avi Brach-Neufeld <brachneufeld@gmail.com>
2025-06-04 10:25:36 -0400

    Document run_trials function

--- coordination_testing/coordination_testing_helpers.py
+++ coordination_testing/coordination_testing_helpers.py
@@ -2,7 +2,16 @@ import datetime
 from openai import OpenAI
 import pandas as pd
 
-def run_trials(models,prompt_dict,number_of_trials,temperature=1,filename=""):
+def run_trials(models,prompt_dict,number_of_trials,temperature=1,filepath=""):
+    """
+    Runs trials for the specified models and prompts and returns dataframe with response texts and trial info. If filepath is set, saves csv file.
+    :param models: List of openAI models to prompt
+    :param prompt_dict: Dictionary with keys of prompt names and values of prompt text to prompt models with
+    :param number_of_trials: Number of trials per prompt/model combination to run.
+    :param temperature: Temperature to pass to model. Default: 1
+    :param filename: Filepath to save data in csv form. If not set, data is not saved. Default:""
+    :returns: Dataframe with trial results and info.
+    """
     client = OpenAI()
     trials=[]
     for model in models:
@@ -20,6 +29,6 @@ def run_trials(models,prompt_dict,number_of_trials,temperature=1,filename=""):
                 trials.append(trial)
         df=pd.DataFrame(trials)
         df['date']=datetime.datetime.now().date()
-        if filename!="":
-            df.to_csv(f'Outputs/{filename}.csv',index=False)
+        if filepath!="":
+            df.to_csv(filepath,index=False)
         return df
